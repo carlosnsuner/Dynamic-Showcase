82d2360a1625225e5a718885b25bdf4a4fd75c18
Carlos Navarro Suñer <carlosnsuner@gmail.com>
2022-09-24 09:56:38 +0200

    Calcular las habilidades de lo jugadores de la selección española

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 
 let totalCarrito1 = document.getElementById("totalCarrito1");
-let precioTotal = 0;
-totalCarrito.innerHTML = `${precioTotal} €`;
+let precioTotal1 = 0;
+totalCarrito1.innerHTML = `${precioTotal1} €`;
 
 //Declaramos el objeto
 // Jugadores de la selección Epañola.
 
-let objetos = [
+let objetos1 = [
     {
         id: "unaisimon",
         precio: 8
@@ -78,115 +78,110 @@ let objetos = [
     {
         id: "sarabia",
         precio: 6
-    },
-
-    // Jugadores de la selección Brasileña.
-
-    {
-        id: "alisson",
-        precio: 8
-    },
-    {
-        id: "ederson",
-        precio: 5
-    },
-    {
-        id: "alexsandro",
-        precio: 7
-    },
-    {
-        id: "danilo",
-        precio: 5
-    },
-    {
-        id: "edermilitao",
-        precio: 7
-    },
-    {
-        id: "marquinhos",
-        precio: 7
-    },
-    {
-        id: "thiagosilva",
-        precio: 9
-    },
-    {
-        id: "casemiro",
-        precio: 9
-    },
-    {
-        id: "philippecoutinho",
-        precio: 8
-    },
-    {
-        id: "fabinho",
-        precio: 7
-    },  
-    {
-        id: "fred",
-        precio: 8
-    },
-    {
-        id: "lucaspaqueta",
-        precio: 6
-    },
-    {
-        id: "antony",
-        precio: 7
-    },
-    {
-        id: "gabrieljesus",
-        precio: 6
-    },
-    {
-        id: "neymarjr",
-        precio: 9
-    },
-    {
-        id: "raphinha",
-        precio: 7
-    },
-    {
-        id: "richarlison",
-        precio: 6
-    },
-    {
-        id: "viniciusjr",
-        precio: 9
-    },
-
-
-
-
-
-
-
-
-
+    }
 ];
 
+    const drag = (ev) => {
+        ev.dataTransfer.setData("text", ev.target.id);
+    
+        // console.log("arrastrando...", ev.target.id);
+    };
+    
+    const allowDrop = (ev) => {
+        ev.preventDefault();
+    };
+    
+    const drop = (ev) => {
+        ev.preventDefault();
+        let data = ev.dataTransfer.getData("text");
+        // ev.target.appendChild(document.getElementById(data));
+        
+        let objetoDeseo = objetos1.find(objeto => {
+            return objeto.id == data
+        });
+    
+        precioTotal1 += objetoDeseo.precio;
+    
+        totalCarrito1.innerHTML = `${precioTotal1} €`;
+    };
 
-
-const drag = (ev) => {
-    ev.dataTransfer.setData("text", ev.target.id);
-
-    // console.log("arrastrando...", ev.target.id);
-};
-
-const allowDrop = (ev) => {
-    ev.preventDefault();
-};
-
-const drop = (ev) => {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    // ev.target.appendChild(document.getElementById(data));
-
-    let objetoDeseo = objetos.find(objeto => {
-        return objeto.id == data
-    });
-
-    precioTotal += objetoDeseo.precio;
-
-    totalCarrito.innerHTML = `${precioTotal} €`;
-};
+// -------------------------
+// let totalCarrito2 = document.getElementById("totalCarrito1");
+// let precioTotal2 = 0;
+// totalCarrito2.innerHTML = `${precioTotal} €`;
+// ];
+// // Jugadores de la selección Brasileña.
+// let objetos2 = [
+// {
+//     id: "alisson",
+//     precio: 8
+// },
+// {
+//     id: "ederson",
+//     precio: 5
+// },
+// {
+//     id: "alexsandro",
+//     precio: 7
+// },
+// {
+//     id: "danilo",
+//     precio: 5
+// },
+// {
+//     id: "edermilitao",
+//     precio: 7
+// },
+// {
+//     id: "marquinhos",
+//     precio: 7
+// },
+// {
+//     id: "thiagosilva",
+//     precio: 9
+// },
+// {
+//     id: "casemiro",
+//     precio: 9
+// },
+// {
+//     id: "philippecoutinho",
+//     precio: 8
+// },
+// {
+//     id: "fabinho",
+//     precio: 7
+// },  
+// {
+//     id: "fred",
+//     precio: 8
+// },
+// {
+//     id: "lucaspaqueta",
+//     precio: 6
+// },
+// {
+//     id: "antony",
+//     precio: 7
+// },
+// {
+//     id: "gabrieljesus",
+//     precio: 6
+// },
+// {
+//     id: "neymarjr",
+//     precio: 9
+// },
+// {
+//     id: "raphinha",
+//     precio: 7
+// },
+// {
+//     id: "richarlison",
+//     precio: 6
+// },
+// {
+//     id: "viniciusjr",
+//     precio: 9
+// },
+// ];
